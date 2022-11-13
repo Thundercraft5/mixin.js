c7b0bfb306ec7efb9dd06528f53132b8c20a3d51
Thundercraft5 <69543685+Thundercraft5@users.noreply.github.com>
2022-11-12 20:11:54 -0500

    .

--- dist/types/utils/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { default as isMixinClass } from "./isMixinClass";
-export { default as isMixinPrototype } from "./isMixinPrototype";
-export { default as makeInstanceOf } from "./makeInstanceOf";
-export { default as mixinInstanceOf } from "./mixinInstanceOf";
-//# sourceMappingURL=index.d.ts.map
